13fde8e349ec8590c23a108786a2ff9eae5faaa5
llorracc <git@llorracc.org>
2025-12-06 22:46:34 -0500

    gh-pages: Single commit from HAFiscal-Latest 2025-12-07T03:46:33Z

---
+++ docs/reproduce.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# To reproduce all the computational results of the paper:
+
+./reproduce/reproduce_computed.sh
+
+# To reproduce a minimal set of the computational results
+
+./reproduce/reproduce_computed_min.sh
+
+# To reproduce the text of the document:
+# - assuming that you have a full installation of texlive on your computer
+
+./reproduce_document.sh
